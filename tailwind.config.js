918da787814e61a6141134b80f07da62f3685bdd
AhmedNourElnabi <91994457+nourelnaby4@users.noreply.github.com>
2025-01-08 06:52:15 +0200

    release

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'color-primary':'#01051e',
+        'color-primary-light':'#020726',
+        'color-primary-dark':'#010417',
+        'color-secondary':'#ff7d3b',
+        'color-gray':'#333',
+        'color-gray-light':'#d1d5db',
+        'color-white':'#fff',
+        'color-blob':'#A427DF',
+      },
+      container: {
+        center: true,
+        padding: '2rem',
+        screens: {
+          sm: '100%', 
+          md: '740px', 
+          lg: '960px', 
+          xl: '1120px', 
+        },
+      },
+      fontFamily: {
+        'myfont-body': ['Poppins', 'Arial', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
